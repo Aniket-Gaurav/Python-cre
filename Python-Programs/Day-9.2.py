a5a850e386ed169493b86c96d7f281abe994ac9f
Aniket Gaurav <aniketgaurav006@gmail.com>
2025-02-19 23:35:18 +0530

    Dictionary with List

--- Python-Programs/Day-9.2.py
+++ Python-Programs/Day-9.2.py
@@ -5,29 +5,30 @@ Student_data = [
 ]
 
 # Print Ram's data
-print(Student_data[0])
+print(Student_data[0]) # Print Ram's data
 
 # Print Mohan's roll number
-print(Student_data[1]["roll_no"])
+print(Student_data[1]["roll_no"]) # Print Mohan's roll number
+
 # Add a phone number to Ram's data
 Student_data[0]["phone_no"] = 9079797
-print(Student_data[0])
+print(Student_data[0]) # Add a phone number to Ram's data
 
 # Delete Ram's phone number
 del Student_data[0]["phone_no"]
-print(Student_data[0])
+print(Student_data[0]) # Delete Ram's phone number
 
 
 # <-------------------------------------------------------------------------------->
 
 # travel_data is a dictionary that stores travel data
 travel_data = {
-    "Gujrat": ["Dwarkadhis", "Somnath", "Statu of Unity"],  # Gujarat travel data
+    "Gujrat": ["Dwarkadhis", "Somnath", "Statue of Unity"],  # Gujarat travel data
     "Maharashtra": ["Pune", "Mumbai", "Nashik"],  # Maharashtra travel data
     "Rajasthan": ["Jaipur", "Udaipur", "Ajmer"],  # Rajasthan travel data
 }
-print(travel_data)
-print(travel_data["Gujrat"][2])  # Print Statue of Unity from Gujarat travel data
+print(travel_data) # Print travel data
+print(travel_data["Gujrat"][2]) # Print Statue of Unity from Gujarat travel data
 
 
 # <-------------------------------------------------------------------------------->
@@ -39,7 +40,21 @@ Student_data = [
 ]
 
 # Print Ram's name
-print(Student_data[0]["Name"])
+print(Student_data[0]["Name"]) # Print Ram's name
 
 # Print Mohan's roll number
-print(Student_data[1]["roll_no"])
+print(Student_data[1]["roll_no"]) # Print Mohan's roll number
+
+# Function to add a new student
+def add_new_student(name, roll_no, marks, grade):
+    new_student = {}
+    new_student["Name"] = name
+    new_student["roll_no"] = roll_no
+    new_student["marks"] = marks
+    new_student["grade"] = grade
+    Student_data.append(new_student)
+
+add_new_student("Shyam", 22, 18, "A+") # Add a new student
+
+print(Student_data) # Print Student_data
+
